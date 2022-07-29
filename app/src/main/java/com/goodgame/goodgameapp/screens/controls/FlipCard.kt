ac5dfec7dc2bb2c3528693abc405b2f0ebc62525
TBoPok <ezhovkinroma@ya.ru>
2022-07-29 08:57:07 +0300

    Стрелки и поворотные карточки

---
+++ app/src/main/java/com/goodgame/goodgameapp/screens/controls/FlipCard.kt
@@ -0,0 +1,72 @@
+package com.goodgame.goodgameapp.screens.controls
+
+import androidx.compose.animation.core.FastOutSlowInEasing
+import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.animation.core.tween
+import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.combinedClickable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.Card
+import androidx.compose.material.ExperimentalMaterialApi
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.graphicsLayer
+
+enum class CardFace(val angle: Float) {
+    Front(0f) {
+        override val next: CardFace
+            get() = Back
+    },
+    Back(180f) {
+        override val next: CardFace
+            get() = Front
+    };
+
+    abstract val next: CardFace
+}
+
+@OptIn(ExperimentalMaterialApi::class, ExperimentalFoundationApi::class)
+@Composable
+fun FlipCard(
+    cardFace: CardFace,
+    modifier: Modifier = Modifier,
+    back: @Composable () -> Unit = {},
+    front: @Composable () -> Unit = {},
+) {
+    val rotation = animateFloatAsState(
+        targetValue = cardFace.angle,
+        animationSpec = tween(
+            durationMillis = 400,
+            easing = FastOutSlowInEasing,
+        )
+    )
+    Box(
+        modifier = modifier
+            .graphicsLayer {
+                rotationY = rotation.value
+                cameraDistance = 12f * density
+            },
+    ) {
+        if (rotation.value <= 90f) {
+            Box(
+                Modifier.fillMaxSize()
+            ) {
+                front()
+            }
+        } else {
+            Box(
+                Modifier
+                    .fillMaxSize()
+                    .graphicsLayer {
+                        rotationY = 180f
+                    },
+            ) {
+                back()
+            }
+        }
+    }
+
+}
